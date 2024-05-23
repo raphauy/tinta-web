b7053b93a569a63bdf3e4564126bf46eb6613fc5
raphauy <rapha.uy@rapha.uy>
2024-05-23 10:06:25 -0300

    clients added

---
+++ src/app/[locale]/clients.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { Card } from "@/components/ui/card"
+import { useTheme } from "next-themes";
+import Image from "next/image"
+import { useEffect, useState } from "react";
+
+const clients = [
+  {
+    name: "BdC",
+  },
+  {
+    name: "BdM",
+  },
+  {
+    name: "Bizarra",
+  },
+  {
+    name: "CerroChapeu",
+  },
+  {
+    name: "Deicas",
+  },
+  {
+    name: "El_Capricho",
+  },
+  {
+    name: "Jomi",
+  },
+  {
+    name: "Marichal",
+  },
+  {
+    name: "Nakkal",
+  },
+  {
+    name: "Narbona",
+  },
+  {
+    name: "Pisano",
+  },
+  {
+    name: "Spinoglio",
+  },
+  {
+    name: "Traversa",
+  },
+  {
+    name: "WoU",
+  },
+];
+
+export function Clients() {
+
+  const [imageColor, setimageColor] = useState("N")
+  const theme= useTheme()
+
+  useEffect(() => {
+    console.log("setting image color")
+    
+    if (theme.resolvedTheme === "dark") {
+      setimageColor("B")
+    } else {
+      setimageColor("N")
+    }
+  }, [theme])
+  
+
+  return (
+    <div className="w-full flex flex-col items-center">
+      <h2 className="text-4xl font-semibold text-center text-tinta-verde mb-14 dark:text-white">Nuestros clientes</h2>
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-2">
+      {
+        clients.map((client) => (
+          <Card key={client.name} className="p-4 flex flex-col items-center justify-center w-32 h-32">
+            <Image src={`/clients/${client.name}_${imageColor}.png`} alt={client.name} width={200} height={200} />
+          </Card>
+        ))
+      }
+      </div>
+    </div>
+  );
+}
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,8 @@
 import { getScopedI18n } from "@/locales/server";
-import DevelopmentBox from "./development-box";
 import Hero from "./hero";
-import SectionBox from "./section-box";
 import Services from "./services";
 import { Testimonials } from "./testimonials";
+import { Clients } from "./clients";
 
 export default async function LanguagePage() {
 
@@ -14,14 +13,19 @@ export default async function LanguagePage() {
 
       <Hero />
 
-      <Services />
+      <div className="w-full max-w-[1000px] mx-auto">
+        <Services />
 
-      <section className="w-full py-6 md:py-12 text-gray-800 dark:text-white mt-10">
-        <h2 className="text-4xl font-semibold text-center text-tinta-verde mb-8 dark:text-white">{tLastSection("title")}</h2>
-        <p className="container text-center text-lg mb-8">{tLastSection("description")}</p>
-      </section>
+        <section className="w-full py-6 md:py-12 text-gray-800 dark:text-white mt-10">
+          <h2 className="text-4xl font-semibold text-center text-tinta-verde mb-8 dark:text-white">{tLastSection("title")}</h2>
+          <p className="container text-center text-lg mb-8">{tLastSection("description")}</p>
+        </section>
 
-      {/* <Testimonials /> */}
+        <Clients />
+
+        {/* <Testimonials /> */}
+
+      </div>
 
     </div>
   )
