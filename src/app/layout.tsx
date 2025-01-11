4e3197a1382df784b7a4e3ad6b16de5fdb2635c1
raphauy <rapha.uy@rapha.uy>
2025-01-11 09:54:41 -0300

    meta pixel added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,55 +72,55 @@ export default async function RootLayout({ children }: RootLayoutProps) {
                 </div>
               </div>            
               
-              <Script id="chatwoot" strategy="afterInteractive">
-                {`
-                  window.chatwootSettings = {"position":"right","type":"standard","launcherTitle":"Chatea con nosotros"};
-                  (function(d,t) {
-                    var BASE_URL="https://agentes.agency-planner.com";
-                    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-                    g.src=BASE_URL+"/packs/js/sdk.js";
-                    g.defer = true;
-                    g.async = true;
-                    s.parentNode.insertBefore(g,s);
-                    g.onload=function(){
-                      window.chatwootSDK.run({
-                        websiteToken: 'muk8sLWJ1mSrhoEWS8iy4nPW',
-                        baseUrl: BASE_URL
-                      })
-                    }
-                  })(document,"script");
-                `}
-              </Script>
-              
-
-              <Script id="facebook-pixel" strategy="afterInteractive">
-                {`
-                  !function(f,b,e,v,n,t,s)
-                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-                  'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', '534601292921561');
-                  fbq('track', 'PageView');
-                `}
-              </Script>
-              <noscript>
-                <Image 
-                  unoptimized
-                  width={1}
-                  height={1}
-                  style={{ display: 'none' }}
-                  src="https://www.facebook.com/tr?id=534601292921561&ev=PageView&noscript=1"
-                  alt=""
-                />
-              </noscript>
 
               <TailwindIndicator />
             </ThemeProvider>
           </SessionProvider>
+          <Script id="chatwoot" strategy="afterInteractive">
+            {`
+              window.chatwootSettings = {"position":"right","type":"standard","launcherTitle":"Chatea con nosotros"};
+              (function(d,t) {
+                var BASE_URL="https://agentes.agency-planner.com";
+                var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                g.src=BASE_URL+"/packs/js/sdk.js";
+                g.defer = true;
+                g.async = true;
+                s.parentNode.insertBefore(g,s);
+                g.onload=function(){
+                  window.chatwootSDK.run({
+                    websiteToken: 'muk8sLWJ1mSrhoEWS8iy4nPW',
+                    baseUrl: BASE_URL
+                  })
+                }
+              })(document,"script");
+            `}
+          </Script>
+          
+
+          <Script id="facebook-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '534601292921561');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript>
+            <Image 
+              unoptimized
+              width={1}
+              height={1}
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=534601292921561&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
         </body>
       </html>
     </>
