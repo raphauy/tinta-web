eb4a219914c1c6315319059b9b1e2004ad49ea9e
raphauy <rapha.uy@rapha.uy>
2025-01-11 09:44:02 -0300

    meta pixel added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import SessionProvider from '@/components/SessionProvider'
 import Header from '@/components/header/header'
 import LanguageToggle from '@/components/header/locale-toggle'
@@ -90,6 +91,31 @@ export default async function RootLayout({ children }: RootLayoutProps) {
                   })(document,"script");
                 `}
               </Script>
+
+              <Script id="facebook-pixel" strategy="afterInteractive">
+                {`
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '534601292921561');
+                  fbq('track', 'PageView');
+                `}
+              </Script>
+              <noscript>
+                <Image 
+                  unoptimized
+                  width={1}
+                  height={1}
+                  style={{ display: 'none' }}
+                  src="https://www.facebook.com/tr?id=534601292921561&ev=PageView&noscript=1"
+                  alt=""
+                />
+              </noscript>
 
               <TailwindIndicator />
             </ThemeProvider>
