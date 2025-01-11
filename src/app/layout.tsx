f728b1a1645ebe9f8263add56ad3ae233a9d6491
raphauy <rapha.uy@rapha.uy>
2025-01-11 09:59:36 -0300

    meta pixel added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,31 +96,6 @@ export default async function RootLayout({ children }: RootLayoutProps) {
             `}
           </Script>
           
-
-          <Script id="facebook-pixel" strategy="afterInteractive">
-            {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '534601292921561');
-              fbq('track', 'PageView');
-            `}
-          </Script>
-          <noscript>
-            <Image 
-              unoptimized
-              width={1}
-              height={1}
-              style={{ display: 'none' }}
-              src="https://www.facebook.com/tr?id=534601292921561&ev=PageView&noscript=1"
-              alt=""
-            />
-          </noscript>
         </body>
       </html>
     </>
